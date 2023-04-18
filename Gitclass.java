700c64102aa0f2e31243985749249cef9fa35f2f
Gonzalo.i <gonzalo.insua11@gmail.com>
2023-04-18 12:47:49 -0300

    commit 4

---
+++ Gitclass.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitclass;
+
+/**
+ *
+ * @author gonza
+ */
+public class Gitclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Caballo;
+        int edad;
+        int cantidad;
+        String color;
+        System.out.println("ingrese edad del caballo");
+        
+        
+         
+    }
+    
+}
