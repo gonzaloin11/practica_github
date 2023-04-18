dd2880a2877294b8243c964e25f408eebc4b9956
Gonzalo.i <gonzalo.insua11@gmail.com>
2023-04-18 16:30:36 -0300

    commit 1 login Gonzalo

--- Gitclass.java
+++ Gitclass.java
@@ -22,6 +22,9 @@ public class Gitclass {
         System.out.println("ingrese edad del caballo");
         System.out.println("prueba github");
         String test;
+        String Gonzalo;
+        String atrib;
+        
         
         
         
